2f86c0b12b427b75355c0597a2421493687a060f
David Wang <cbwang2016@users.noreply.github.com>
2019-01-06 16:01:00 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-//=======================================================================
-// Copyright (c) 2017 Adrian Schneider
-// Distributed under the terms of the MIT License.
-// (See accompanying file LICENSE or copy at
-//  http://opensource.org/licenses/MIT)
-//=======================================================================
-
 #include <iostream>
 #include <vector>
 #include "mnist/mnist_reader.hpp"
